92f2c5a8236ea84323d60de07a7538eb7859ffe9
Criinix <102599308+Criinix@users.noreply.github.com>
2022-05-30 22:13:16 +0200

    update

--- Website/logic.js
+++ Website/logic.js
@@ -1,32 +1,3 @@
-function machWas() {
-    let text = "Hallo";
-    for (let i = 0; i < 100; i++) {
-        text = text + i;
-    }
-    document.getElementById("out").innerHTML = text;
+function IndexAufrufen() {
+    window.location = "http://10.3.141.1/BeerMachine/index.html"
 }
-
-function callWebpage(urlI) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", urlI, true);
-    xhr.send();
-
-    // Callback-funktion - Anfang ------------------
-    // (wird aufgerufen sobald Antwort vom Server da)
-    xhr.onreadystatechange = function() {
-        console.log('ready');
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText !== '') {
-                if (this.responseText < 0) {
-                    testState = -1;
-                    console.log("Error: " + this.responseText);
-                    document.getElementById('state').innerHTML = "Error: " + this.responseText;
-                } else
-                    document.getElementById('state').innerHTML = this.responseText;
-            }
-        } else {
-            console.log('timeout');
-        }
-    };
-}
-// Callback-funktion - Ende ---------------------
