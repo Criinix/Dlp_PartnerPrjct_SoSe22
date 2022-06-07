ed2f0ae060900aca1923d40a45b1de46fd0d9f7c
Throx13 <102957726+Throx13@users.noreply.github.com>
2022-06-07 15:35:09 +0200

    Add files via upload

---
+++ Website/Dateien Niklas/TestWebseite.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+    <h1> Test-HTML-Datei </h1>
+<p>
+    Das ist die erste Testzeile <br>
+    zweite Testzeile
+</p>
+
+<a href="https://www.youtube.com/" target="_blank">Youtube Link</a>
+
+<p> <img src="https://media.os.fressnapf.com/cms/2020/05/Ratgeber-Katze-Gesundheit-KatzeWiese_1200x527.jpg?t=cmsimg_920"> Katze </p>
+
+<p>
+<form method="get">
+    Wunschgetränk: <input type ="text" name="getraenk"></input><br><br>
+    <input type="submit" value="Senden"></input>
+</p>
+
+<br>
+
+<p>
+<form method="get">
+Status: <input type ="text" name="status"></input><br><br>
+<input type="submit" value="Senden"></input>
+</p>
+
+<p>
+<?php
+    $status = $_GET['status'];
+    file_put_contents("MCstatus.txt", $status);
+    echo "Status in Datei MCtestdatei wurde auf $status gesetzt<br>";
+?>
+
+</body>
+</html>
