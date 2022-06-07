f9fed6bec7fcd2c1546ecf073efdc2ea04fbee31
Throx13 <102957726+Throx13@users.noreply.github.com>
2022-06-07 15:32:08 +0200

    Delete MCcom.php

--- Website/Testdateien/MCcom.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET['neuerStatusMC'])) {
-    $neuerStatus = $_GET['neuerStatusMC'];
-    file_put_contents("MCstatus.txt", $neuerStatus);
-}
-
-//Hier Füllstände in Datenbank speichern
-?>
