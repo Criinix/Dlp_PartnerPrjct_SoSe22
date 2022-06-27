ec1a101051df29c96e41a54265f7c17967409cef
Throx13 <102957726+Throx13@users.noreply.github.com>
2022-06-27 22:36:09 +0200

    Add files via upload

---
+++ Website/Dateien_Niklas/APPcom.php
@@ -0,0 +1,34 @@
+<?php
+
+//Status in Datenbank ändern
+if (isset($_GET['neuerStatusAPP'])) {
+    $neuerStatus = $_GET['neuerStatusAPP'];
+    /*$sql ? "UPDATE Mikrocontroller SET Status = $neuerStatus";
+    $result = $db->Query($sql);
+
+    if ($result) {
+        echo "Status aktualisiert";
+    }
+    else {
+        echo "Statusänderung fehlgeschlagen!";
+    
+    }*/
+    file_put_contents("APPcomTest.txt", $neuerStatus); //Test mit Textdatei
+
+}
+
+//Status aus Datenbank abfragen
+if (isset($_GET['Statusabfrage'])) {
+    /*$sql = "SELECT Status FROM Mikrocontroller";
+    $result = $db->query($sql);
+    if ($result) {
+        while ($datensatz = $result->fetch_object()) {
+            echo $datensatz->Status;
+        }
+    }
+    else {
+        echo "Lesen fehlgeschlagen!<br>";
+    }*/
+    echo file_get_contents('APPcomTest.txt');
+}
+?>
